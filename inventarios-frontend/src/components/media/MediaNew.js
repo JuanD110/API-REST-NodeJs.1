90b6c2247039155260c2d40b1e27a12b76006160
JuanD110 <david.galeano@est.iudigital.edu.co>
2024-09-29 19:02:46 -0500

    cambios

--- inventarios-frontend/src/components/media/MediaNew.js
+++ inventarios-frontend/src/components/media/MediaNew.js
@@ -1,87 +1,113 @@
 import React, { useEffect, useState } from 'react';
 import { getDirectores } from '../../services/directorService';
 import { getGeneros } from '../../services/generoService';
+import { crearMedia } from '../../services/mediaService';
 import { getProductora } from '../../services/productoraService';
 import { getTipo } from '../../services/tipoService';
 
-export const MediaNew = ({ handleOpenModal }) => {
+
+export const MediaNew = ({ handleOpenModal, listarMedia }) => {
   
-  const [generos, setGeneros ] = useState([]);
-  const [directores, setDirectores ] = useState([]);
-  const [Productoras, setProductoras ] = useState([]);
-  const [tipos, setTipos ] = useState([]);
-  const [valoresForm, setValoresForm ] = useState([]);
-  const {serial = '', titulo = '', sinopsis = '', url = '', imagenPortada = '',
-    fechaCreacion = '', fechaActualizacion = '', añoEstreno = '', 
-    generoPrincipal, directorPrincipal, productora, tipo 
-  } = valoresForm
+  const [generos, setGeneros] = useState([]);
+  const [directores, setDirectores] = useState([]);
+  const [productoras, setProductoras] = useState([]);
+  const [tipos, setTipos] = useState([]);
+  const [valoresForm, setValoresForm] = useState({
+    serial: '', titulo: '', sinopsis: '', url: '', imagenPortada: '',
+    fechaCreacion: '', fechaActualizacion: '', añoEstreno: '',
+    generoPrincipal: '', directorPrincipal: '', productora: '', tipo: ''
+  });
+
+  const {
+    serial, titulo, sinopsis, url, imagenPortada,
+    fechaCreacion, fechaActualizacion, añoEstreno,
+    generoPrincipal, directorPrincipal, productora, tipo
+  } = valoresForm;
 
   const listarDirectores = async () => {
-    try{
-        const { data } = await getDirectores();
-        setDirectores(data);
-
-    } catch(error){
+    try {
+      const { data } = await getDirectores();
+      setDirectores(data);
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(() => {
-      listarDirectores();
-  
-}, []);
-  
-const listarGeneros = async () => {
-  try{
+  const listarGeneros = async () => {
+    try {
       const { data } = await getGeneros();
       setGeneros(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  } catch(error){
-    console.log(error);
-  }
-}
-
-useEffect(() => {
-    listarGeneros();
-
-}, []);
-
-const listarTipo = async () => {
-  try{
+  const listarTipo = async () => {
+    try {
       const { data } = await getTipo();
       setTipos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  } catch(error){
-    console.log(error);
-  }
-}
-
-useEffect(() => {
-    listarTipo();
-
-}, []);
-
-const listarProductora = async () => {
-  try{
+  const listarProductora = async () => {
+    try {
       const { data } = await getProductora();
       setProductoras(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  } catch(error){
-    console.log(error);
-  }
-}
-
-useEffect(() => {
+  useEffect(() => {
+    listarDirectores();
+    listarGeneros();
+    listarTipo();
     listarProductora();
+  }, []);
 
-}, []);
+  const handleOnChange =  ({ target }) => {
+    const { name, value } = target;
+    setValoresForm({ ...valoresForm, [name]: value });
+  };
 
-const handleOnChange = ({ target }) => {
-  const { name, value } = target;
-  setValoresForm({...valoresForm, [name]: value });
-}
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const media = {
+      serial,
+      titulo,
+      sinopsis,
+      url,
+      imagenPortada,
+      fechaCreacion,
+      fechaActualizacion,
+      añoEstreno,
+      generoPrincipal: {
+        _id: generoPrincipal 
+      },
+      directorPrincipal: {
+        _id: directorPrincipal 
+      },
+      productora: {
+        _id: productora 
+      },
+      tipo: {
+        _id: tipo 
+      }
+    };
 
-return (
+    try {
+      await crearMedia(media);
+      console.log("Media guardada correctamente");
+      handleOpenModal(); 
+    } catch (error) {
+      console.log("Error al guardar media", error);
+    }
+};
+
+
+  return (
     <div className='sidebar'>
       <div className="container-fluid">
         <div className='row'>
@@ -99,47 +125,38 @@ return (
           </div>
         </div>
 
-        <form>
+        <form onSubmit={(e) => handleOnSubmit(e)}>
           <div className='row'>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Serial</label>
                 <input type="text" name='serial' 
-                value = {serial}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"   />
-
+                  value={serial} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Titulo</label>
                 <input type="text" name='titulo'
-                value = {titulo}
-                onChange={ e => handleOnChange(e)} 
-                required 
-                className="form-control"  />
+                  value={titulo} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Sinopsis</label>
                 <input type="text" name='sinopsis' 
-                value = {sinopsis}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={sinopsis} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Url</label>
                 <input type="text" name='url' 
-                value = {url}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={url} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
           </div>
@@ -149,40 +166,32 @@ return (
               <div className="mb-3">
                 <label className="form-label">Imagen Portada</label>
                 <input type="text" name='imagenPortada' 
-                value = {imagenPortada}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={imagenPortada} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Fecha Creacion</label>
                 <input type="date" name='fechaCreacion' 
-                value = {fechaCreacion}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={fechaCreacion} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Fecha Actualizacion</label>
                 <input type="date" name='fechaActualizacion' 
-                value = {fechaActualizacion}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={fechaActualizacion} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Año Estreno</label>
                 <input type="text" name='añoEstreno' 
-                value = {añoEstreno}
-                onChange={ e => handleOnChange(e)}
-                required 
-                className="form-control"  />
+                  value={añoEstreno} onChange={handleOnChange} required 
+                  className="form-control" />
               </div>
             </div>
           </div>
@@ -191,15 +200,13 @@ return (
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Genero Principal</label>
-                <select class="form-select"
-                required
-                name= 'generoPrincipal'
-                value = {generoPrincipal}
-                onChange={ e => handleOnChange(e)}>
-                <option value = "">---SELECCIONE---</option>
+                <select className="form-select"
+                  required name='generoPrincipal'
+                  value={generoPrincipal} onChange={handleOnChange}>
+                  <option value="">---SELECCIONE---</option>
                   {
-                    generoPrincipal.map(({_id, nombres}) =>{
-                      return <option key={_id}>{nombres}</option>
+                    generos.map(({ _id, nombres }) => {
+                      return <option key={_id} value={_id}>{nombres}</option>
                     })
                   }
                 </select>
@@ -208,15 +215,13 @@ return (
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Director Principal</label>
-                <select class="form-select"
-                required
-                name= 'directorPrincipal'
-                value = {directorPrincipal}
-                onChange={ e => handleOnChange(e)}>
-                <option value = "">---SELECCIONE---</option>
+                <select className="form-select"
+                  required name='directorPrincipal'
+                  value={directorPrincipal} onChange={handleOnChange}>
+                  <option value="">---SELECCIONE---</option>
                   {
-                    directorPrincipal.map(({_id, nombres}) =>{
-                      return <option key={_id}>{nombres}</option>
+                    directores.map(({ _id, nombres }) => {
+                      return <option key={_id} value={_id}>{nombres}</option>
                     })
                   }
                 </select>
@@ -225,15 +230,13 @@ return (
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Productora</label>
-                <select class="form-select"
-                required
-                name= 'productora'
-                value = {productora}
-                onChange={ e => handleOnChange(e)}>
-                <option value = "">---SELECCIONE---</option>
+                <select className="form-select"
+                  required name='productora'
+                  value={productora} onChange={handleOnChange}>
+                  <option value="">---SELECCIONE---</option>
                   {
-                    productora.map(({_id, nombres}) =>{
-                      return <option key={_id}>{nombres}</option>
+                    productoras.map(({ _id, nombres }) => {
+                      return <option key={_id} value={_id}>{nombres}</option>
                     })
                   }
                 </select>
@@ -242,19 +245,22 @@ return (
             <div className='col'>
               <div className="mb-3">
                 <label className="form-label">Tipo</label>
-                <select class="form-select"
-                required
-                name= 'tipo'
-                value = {tipo}
-                onChange={ e => handleOnChange(e)}>
-                <option value = "">---SELECCIONE---</option>
+                <select className="form-select"
+                  required name='tipo'
+                  value={tipo} onChange={handleOnChange}>
+                  <option value="">---SELECCIONE---</option>
                   {
-                    tipos.map(({_id, nombres}) =>{
-                      return <option key={_id}>{nombres}</option>
+                    tipos.map(({ _id, nombres }) => {
+                      return <option key={_id} value={_id}>{nombres}</option>
                     })
                   }
                 </select>
               </div>
+            </div>
+          </div>
+          <div className= 'row'>
+            <div className= 'col'>
+              <button type= "button" className= "btn btn-primary">Guardar</button>
             </div>
           </div>
         </form>
@@ -262,5 +268,3 @@ return (
     </div>
   );
 };
-
-
--- inventarios-frontend/src/components/ui/Header.js
+++ inventarios-frontend/src/components/ui/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <NavLink className="navbar-brand" exact to='/'>PelisPlay</NavLink>
+            <NavLink className="navbar-brand" exact to='/media'>PelisPlay</NavLink>
     
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
